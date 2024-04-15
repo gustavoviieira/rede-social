ca21a4516d52e9a7f6981a1fcb3f981921b0c74a
Gustavo Vieira <Gustavos3654@gmail.com>
2024-04-14 22:35:02 -0300

    first commit

---
+++ src/scripts/dataBase.js
@@ -0,0 +1,67 @@
+const users = [
+    {
+        id: 1,
+        user: "Samuel Leão",
+        stack: "Front end Engineer",
+        img: "./src/assets/images/user1.svg",
+    },
+];
+const posts = [
+    {
+        id: 1,
+        title: "Como criar um interface simples e agradável utilizando boas práticas de design",
+        text: "Hoje vamos conversar sobre como criar uma interface agradável mesmo sem ter um design pronto feito por um profissional de UI design.",
+        user: "Samuel Leão",
+        stack: "Front end Engineer",
+        img: "./src/assets/images/user1.svg",
+        likes: "25",
+    },
+    {
+        id: 2,
+        title: " Empresa de Tecnologia da Informação abre vagas para programa de estágio",
+        text: "A Generic Digital, empresa natalence especializada em Tecnologia da Informação, irá iniciar o seu sexto programa de estágio, com o prazo de inscrições entre os dias 10 e 28 de agosto. ",
+        user: "Samuel Persuhn",
+        stack: "Front end Engineer",
+        img: "./src/assets/images/user2.svg",
+        likes: "25",
+    },
+    {
+        id: 3,
+        title: " Como melhorar sua lógica de programação? Desenvolvemos 5 passos para te ajudar",
+        text: "Para quem está começando na área de T.I, em pleno século 21 este conceito pode parecer um grande desafio, pois requer muitas habilidades em diversas áreas de estudo, principalmente...",
+        user: "Carla Maria",
+        stack: "Back end Engineer",
+        img: "./src/assets/images/user3.svg",
+        likes: "19",
+    },
+    {
+        id: 4,
+        title: "NextJS como essa ferramenta revolucionou a maneira de desenvolver sites",
+        text: "Provavelmente, você já ouviu falar do Next.js, considerado o framework mais popular do mercado. Com a expansão do mercado de tecnologia e sua influência em serviços de diver...",
+        user: "Júlia Martins",
+        stack: "Devop's",
+        img: "./src/assets/images/user4.svg",
+        likes: "19",
+    },
+];
+
+const sugestsUsers = [
+    {
+        id: 1,
+        user: "Carlos Lima",
+        stack: "UX e UI Designer",
+        img: "./src/assets/images/usersu1.svg",
+    },
+    {
+        id: 2,
+        user: "Larissa Carmago",
+        stack: "UX e UI Designer",
+        img: "./src/assets/images/usersu2.svg",
+    },
+    {
+        id: 3,
+        user: "Filipe Guitierry",
+        stack: "Full Stack Developer",
+        img: "./src/assets/images/usersu3.svg",
+    },
+];
---
+++ src/scripts/script.js
@@ -0,0 +1,125 @@
+function createPost(users) {
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+
+        const perfilContainer = document.querySelector(".perfil__container");
+        const img = document.createElement("img");
+        const perfilInfo = document.createElement("div");
+        const stack = document.createElement("h2");
+        const name = document.createElement("p");
+
+        perfilInfo.classList.add("perfil__info");
+        img.src = user.img;
+        name.innerText = user.user;
+        name.classList.add("perfil__name");
+        stack.innerText = user.stack;
+        stack.classList.add("perfil__stack");
+
+        perfilInfo.append(name, stack);
+        perfilContainer.append(img, perfilInfo);
+    }
+}
+function createSectionSugest(sugestsUsers) {
+    for (let i = 0; i < sugestsUsers.length; i++) {
+        let sugestsUser = sugestsUsers[i];
+
+        const sugestPerfilContainer = document.querySelector(
+            ".sugest__perfil-container"
+        );
+        const sugestPerfil = document.createElement("div");
+        const sugestUserConteiner = document.createElement("div");
+        const sugestImg = document.createElement("img");
+        const sugestInfo = document.createElement("div");
+        const name = document.createElement("h2");
+        const stack = document.createElement("p");
+        const divButton = document.createElement("div");
+        const button = document.createElement("button");
+
+        sugestPerfil.classList.add("sugest__perfil");
+        sugestUserConteiner.classList.add("sugest__user");
+        sugestImg.classList.add("sugest__img");
+        sugestImg.src = sugestsUser.img;
+        sugestInfo.classList.add("sugest__info");
+        name.innerText = sugestsUser.user;
+        name.classList.add("perfil__name");
+        stack.innerText = sugestsUser.stack;
+        stack.classList.add("perfil__stack");
+        divButton.classList.add("btn");
+        button.classList.add("active");
+        button.innerText = "Seguir";
+
+        button.addEventListener("click", () => {
+            button.classList.toggle("disable");
+        });
+
+        sugestInfo.append(name, stack);
+        divButton.append(button);
+        sugestUserConteiner.append(sugestImg, sugestInfo);
+        sugestPerfil.append(sugestUserConteiner, divButton);
+        sugestPerfilContainer.append(sugestPerfil);
+    }
+}
+function createPostSection(posts) {
+    for (let i = 0; i < posts.length; i++) {
+        let post = posts[i];
+        const postsContainerUser = document.querySelector(
+            ".posts__container-user"
+        );
+        const postsUser = document.createElement("div");
+        const imgUser = document.createElement("img");
+        const postInfo = document.createElement("div");
+        const name = document.createElement("div");
+        const stack = document.createElement("div");
+        const postTitle = document.createElement("h2");
+        const text = document.createElement("p");
+        const buttonContainer = document.createElement("div");
+        const button = document.createElement("button");
+        const likeContainer = document.createElement("div");
+        const imgLike = document.createElement("img");
+        const imgLiked = document.createElement("img");
+        const contagem = document.createElement("p");
+
+        postsUser.classList.add("posts__user");
+        imgUser.src = post.img;
+        postInfo.classList.add("posts__info");
+        name.classList.add("posts__name");
+        name.innerText = post.user;
+        stack.classList.add("posts__stack");
+        stack.innerText = post.stack;
+        postTitle.classList.add("posts__title");
+        postTitle.innerText = post.title;
+        text.classList.add("posts__text");
+        text.innerText = post.text;
+        buttonContainer.classList.add("button__container");
+        button.classList.add("post__button");
+        button.innerText = "Abrir Post";
+        likeContainer.classList.add("likeConteiner");
+
+        imgLiked.classList.add("liked");
+        contagem.innerText = post.likes;
+        contagem.classList.add("n_like");
+
+        imgLike.src = "./src/assets/images/like.svg";
+
+        imgLike.addEventListener("click", () => {
+            imgLike.classList.toggle("liked");
+        
+            if (imgLike.classList.contains("liked")) {
+                imgLike.src = "./src/assets/images/liked.svg";
+                contagem.innerText = Number(contagem.innerText) + 1;
+            } else {
+                imgLike.src = "./src/assets/images/like.svg";
+                contagem.innerText = Number(contagem.innerText) - 1;
+            }
+        });
+
+        postsContainerUser.append(postsUser, postTitle, text, buttonContainer);
+        postsUser.append(imgUser, postInfo);
+        postInfo.append(name, stack);
+        buttonContainer.append(button, likeContainer);
+        likeContainer.append(imgLike, contagem);
+    }
+}
+createPostSection(posts);
+createSectionSugest(sugestsUsers);
+createPost(users);
